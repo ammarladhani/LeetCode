fa7ce91d0bdd4e439802a966f4e47964c27a8032
Ammar Ladhani <55207849+ammarladhani@users.noreply.github.com>
2024-05-10 17:07:57 -0400

    Time: 5 ms (8.67%), Space: 5.2 MB (92.54%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.c
@@ -0,0 +1,14 @@
+int scoreOfString(char* s) {
+    int i = 1;
+    int score = 0;
+    while(s[i]) {
+        int adding = s[i]-s[i-1];
+        printf("%d", adding);
+        if(adding < 0) {
+            adding *= -1;
+        }
+        score+=adding;
+        i++;
+    }
+    return score;
+}
