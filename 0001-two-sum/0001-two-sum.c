d57db43bd6252a18bf4095051284a124549a827b
Ammar Ladhani <55207849+ammarladhani@users.noreply.github.com>
2024-05-16 14:51:38 -0400

    Time: 0 ms (100.00%), Space: 7.8 MB (5.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -2,16 +2,28 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    struct hashTable {
+        int key;
+        int value;
+        UT_hash_handle hh;
+    } *hashTable = NULL, *item, *tmpItem;
     *returnSize = 2;
     for (int i = 0; i < numsSize; i++) {
-        for (int j = i + 1; j < numsSize; j++) {
-            if (nums[i]+nums[j] == target) {
-                int* result = malloc(sizeof(int) * 2);
-                result[0] = i;
-                result[1] = j;
-                return result;
+        HASH_FIND_INT(hashTable, &nums[i], item);
+        if (item) {
+            int* result = malloc(sizeof(int) * 2);
+            result[0] = item->value;
+            result[1] = i;
+            HASH_ITER(hh, hashTable, item, tmpItem) {
+                HASH_DEL(hashTable, item);
+                free(item);
             }
+            return result;
         }
+        item = malloc(sizeof(struct hashTable));
+        item->key = target - nums[i];
+        item->value = i;
+        HASH_ADD_INT(hashTable, key, item);
     }
     return NULL;
 }
