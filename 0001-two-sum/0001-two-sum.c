32850e4cf2de7133f3dda0bdc6441bb345637085
Ammar Ladhani <55207849+ammarladhani@users.noreply.github.com>
2024-05-16 14:43:22 -0400

    Time: 10 ms (92.01%), Space: 7.5 MB (5.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -7,14 +7,13 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
         int value;
         UT_hash_handle hh;
     } *hashTable = NULL, *item, *tmpItem;
-
+    *returnSize = 2;
     for (int i = 0; i < numsSize; i++) {
         HASH_FIND_INT(hashTable, &nums[i], item);
         if (item) {
             int* result = malloc(sizeof(int) * 2);
             result[0] = item->value;
             result[1] = i;
-            *returnSize = 2;
             HASH_ITER(hh, hashTable, item, tmpItem) {
                 HASH_DEL(hashTable, item);
                 free(item);
