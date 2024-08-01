49af550e30b873e36e0e76e096bf77e1e422f26c
Ammar Ladhani <55207849+ammarladhani@users.noreply.github.com>
2024-08-01 15:32:53 -0500

    Time: 28 ms (93.65%), Space: 13.2 MB (65.18%) - LeetHub

---
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def doesAliceWin(self, s):
+        if"a" in s or "e" in s or "i" in s or "o" in s or "u" in s:
+            return True
+        else: 
+            return False
+        
